fd45e61a64e9c53f35273321c8a31398c678225e
a.dalessandro@imperial.ac.uk <a.dalessandro@imperial.ac.uk>
2020-12-14 20:00:49 +0000

    Created AmbitionLevelSlider

--- client_code/Main/AmbitionLevelSlider/__init__.py
+++ client_code/Main/AmbitionLevelSlider/__init__.py
@@ -1,16 +1,16 @@
-from ._anvil_designer import AmbitionLevelTemplate
+from ._anvil_designer import AmbitionLevelSliderTemplate
 from anvil import *
 import anvil.server
 
-class AmbitionLevel(AmbitionLevelTemplate):
+class AmbitionLevelSlider(AmbitionLevelSliderTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run when the form opens.
     self.slider.level = self.value
-
-  def slider_1_change(self, level, **event_args):
+    
+  def slider_change(self, level, **event_args):
     """This method is called when the slider is moved"""
     self.value_label.text = self.value = level
     self.raise_event("changed")
--- client_code/Main/__init__.py
+++ client_code/Main/__init__.py
@@ -3,7 +3,7 @@ from anvil import *
 import anvil.server
 import plotly.graph_objects as go
 
-from .AmbitionLevel import AmbitionLevel
+from .AmbitionLevelSlider import AmbitionLevelSlider as ASlider
 
 class Main(MainTemplate):
     def __init__(self, **properties):
@@ -11,15 +11,20 @@ class Main(MainTemplate):
         self.init_components(**properties)
 
         # Any code you write here will run when the form opens.
-        self.period_input.level = 1
-        self.add_component(AmbitionLevel(name="Period"), slot="")
         self.sin_plot.layout.title = "Sin Graph"
         self.cos_plot.layout.title = "Cos Graph"
+        
+        # Can programatically add components
+        self.period_slider = ASlider(name="Period", max=5, changed=self.build_graphs)
+        self.sliders_column.add_component(self.period_slider)
+        self.period_slider.set_event_handler("changed", self.slider_changed)
+        
         self.build_graphs()
+
 
     def build_graphs(self):
         model_outputs = anvil.server.call(
-            "calculate", self.period_input.level, self.phase_level.value
+            "calculate", self.period_slider.value, self.phase_slider.value
         )
         self.sin_plot.data = go.Scatter(
             x=model_outputs["X"][0],
@@ -34,13 +39,8 @@ class Main(MainTemplate):
             name="cos",
         )
 
-    def period_input_change(self, level, **event_args):
-      """This method is called when the slider is moved"""
-      self.period_value.text = level
-      self.build_graphs()
-
-    def phase_level_changed(self, **event_args):
-      """This method is called Value of slider changed"""
+    def slider_changed(self, **event_args):
+      """This method is called when the value of the slider is changed"""
       self.build_graphs()
 
 
