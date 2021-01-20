9a0dc7fcbf16ccf7338ca9e1acc753925d6ee5d5
Adrian D'Alessandro <a.dalessandro@imperial.ac.uk>
2021-01-20 13:33:35 +0000

    Include i18n in requirements

--- server_code/Model2050Server.py
+++ server_code/Model2050Server.py
@@ -87,3 +87,22 @@ i18n.load_path.append("./Template2050Calculator/server_code/translations")
 def translate(locale, text):
     i18n.set("locale", locale)
     return i18n.t(text)
+
+
+from openpyxl import load_workbook
+
+wb = load_workbook(
+    filename="../UK_Pathways_Calculator_Model_Development_Climact_200703_publ_template.xlsm"
+)
+ws = wb["WebOutputs"]
+
+web_outputs = {}
+for col in ws.iter_cols(3, 14, 31, 68, values_only=True):
+    web_outputs[col[0]] = col[1:]
+
+print(web_outputs.keys())
+
+
+@anvil.server.callable
+def web_outputs_keys():
+    return web_outputs.keys()
