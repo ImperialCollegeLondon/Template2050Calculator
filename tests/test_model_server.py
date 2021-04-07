adad1efa77587612c73a06ef3e712b5ae9828903
Adrian D'Alessandro <a.dalessandro@imperial.ac.uk>
2021-04-07 17:04:08 +0100

    Skip tests when no interface module available

--- tests/test_model_server.py
+++ tests/test_model_server.py
@@ -1,3 +1,13 @@
+from pytest import mark
+
+# Skip this test module when the interface2050 module is unavailable (currently in ci)
+# In future, should be able to pull in the interface from a docker image
+try:
+    import server_code.interface2050  # noqa: F401
+except ImportError:
+    pytestmark = mark.skipif(True, reason="No interface module available.")
+
+
 def test_inputs():
     import numpy as np
     from server_code.Model2050Server import inputs
