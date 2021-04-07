d8b517114d1762b6cbb2edfd1581f3539114f2fc
Adrian D'Alessandro <a.dalessandro@imperial.ac.uk>
2021-04-07 18:45:59 +0100

    Use MagicMock with side_effect

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,16 @@
 from pytest import fixture
-from unittest.mock import patch
+from unittest.mock import MagicMock, patch
 
 
 @fixture(autouse=True)
 def patch_server_call():
-    with patch("anvil.server.call", lambda *x: x):
-        yield
+    def side_effect(*args):
+        if len(args) == 0:
+            return None
+        if len(args) == 1:
+            return args[0]
+        return args
+
+    mocker = MagicMock(side_effect=side_effect)
+    with patch("anvil.server.call", mocker):
+        yield mocker
--- tests/test_client_code.py
+++ tests/test_client_code.py
@@ -1,8 +1,10 @@
-def test_model():
+def test_model(patch_server_call):
     from client_code.Model import language, levers, inputs, outputs, translate
 
     assert language == "en"
-    assert levers[0] == "levers"
-    assert inputs[0] == "inputs"
-    assert outputs[0] == "outputs"
+    assert levers == "levers"
+    assert inputs == "inputs"
+    assert outputs == "outputs"
+
     assert translate("text") == ("translate", language, "text")
+    patch_server_call.assert_called_with("translate", language, "text")
