5f38dfaf88a2a20014e69b9682ca95c3a7bfcfd3
Christopher Cave-Ayland <c.cave-ayland@imperial.ac.uk>
2021-04-21 15:54:50 +0100

    Simplified plotting module

--- client_code/Plots.py
+++ client_code/Plots.py
@@ -1,19 +1,5 @@
 import plotly.graph_objects as go
 
-PLOTS_REGISTRY = {}
-"""Registry of known plots"""
-
-
-def _register_plot(func=None, plot_type=None):
-    """Register a plot."""
-    if func is None:
-        return lambda f: _register_plot(f, plot_type=plot_type)
-
-    plot_type = plot_type if plot_type else func.__name__
-    PLOTS_REGISTRY[plot_type] = func
-
-    return func
-
 
 def _prepare_rows(data, x):
     for row in data[::-1]:
@@ -22,23 +8,25 @@ def _prepare_rows(data, x):
         yield name, trace
 
 
-def _partial_scatter(x, y, plot_type):
-    return lambda **kwargs: go.Scatter(
-        x=x, y=y, plot_type=plot_type, showlegend=True, **kwargs
-    )
+def _partial_scatter(x, y, name, **kwargs):
+    return go.Scatter(x=x, y=y, name=name, showlegend=True, **kwargs)
 
 
-@_register_plot(plot_type="stacked area with overlying line(s)")
 def plot_stacked_area(x, model_output):
     return [
-        _partial_scatter(x, y, plot_type)(mode="lines", stackgroup="one")
-        for plot_type, y in _prepare_rows(model_output, x)
+        _partial_scatter(x, y, name=name, mode="lines", stackgroup="one")
+        for name, y in _prepare_rows(model_output, x)
     ]
 
 
-@_register_plot(plot_type="line")
 def plot_line(x, model_output):
     return [
-        _partial_scatter(x, y, plot_type)(mode="lines+markers")
-        for plot_type, y in _prepare_rows(model_output, x)
+        _partial_scatter(x, y, name, mode="lines+markers")
+        for name, y in _prepare_rows(model_output, x)
     ]
+
+
+PLOTS_REGISTRY = {
+    "stacked area with overlying line(s)": plot_stacked_area,
+    "line": plot_line,
+}
